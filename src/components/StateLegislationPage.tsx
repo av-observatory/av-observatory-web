831b3dfd21c734c0096ab5d1e0d0de99d73183b7
Matthew Raifman <matthew.raifman@gmail.com>
2026-09-24 12:09:48 -0700

    Fix northeast legislation tile positions

--- src/components/StateLegislationPage.tsx
+++ src/components/StateLegislationPage.tsx
@@ -5,9 +5,9 @@ import { displayDate, usePolicyData } from "@/lib/policyTracker";
 import { useLegislationData, type Bill } from "@/lib/legislationTracker";
 
 const TILE_POSITIONS: Record<string,[number,number]> = {
-  ME:[1,12], VT:[2,11], NH:[2,12],
-  WA:[3,1], ID:[3,2], MT:[3,3], ND:[3,4], MN:[3,5], WI:[3,6], MI:[3,7], NY:[3,9], MA:[3,11],
-  OR:[4,1], NV:[4,2], WY:[4,3], SD:[4,4], IA:[4,5], IL:[4,6], IN:[4,7], OH:[4,8], PA:[4,9], NJ:[4,10], CT:[4,11], RI:[4,12],
+  ME:[1,11], VT:[2,10], NH:[2,11],
+  WA:[3,1], ID:[3,2], MT:[3,3], ND:[3,4], MN:[3,5], WI:[3,6], MI:[3,7], NY:[3,9], MA:[3,10], RI:[3,11],
+  OR:[4,1], NV:[4,2], WY:[4,3], SD:[4,4], IA:[4,5], IL:[4,6], IN:[4,7], OH:[4,8], PA:[4,9], NJ:[4,10], CT:[4,11],
   CA:[5,1], UT:[5,2], CO:[5,3], NE:[5,4], MO:[5,5], KY:[5,6], WV:[5,7], VA:[5,8], MD:[5,9], DE:[5,10],
   AZ:[6,2], NM:[6,3], KS:[6,4], AR:[6,5], TN:[6,6], NC:[6,7], SC:[6,8], DC:[6,9],
   OK:[7,4], LA:[7,5], MS:[7,6], AL:[7,7], GA:[7,8],
